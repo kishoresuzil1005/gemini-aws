b63f7ea964619fb7e5afd5097f18af004e9bde89
JEYAPRAKAH21 <jeyaprakashp342@gmail.com>
2026-07-08 15:47:17 +0530

    Implement Wave 3: Storage Upgrades

--- backend/app/providers/aws/ebs.py
+++ backend/app/providers/aws/ebs.py
@@ -1,19 +1,62 @@
 import boto3
+import logging
+from app.providers.aws.models import NormalizedResource, ResourceDependency
+
+logger = logging.getLogger(__name__)
 
 class EBSDiscovery:
-
     @staticmethod
-    def discover(region):
+    def discover(region: str):
         try:
             client = boto3.client('ec2', region_name=region)
-            response = client.describe_volumes()
+            paginator = client.get_paginator('describe_volumes')
             volumes = []
-            for vol in response.get('Volumes', []):
-                attachments_list = []
-                for att in vol.get('Attachments', []):
-                    if 'InstanceId' in att:
-                        attachments_list.append(att['InstanceId'])
-                volumes.append({'resource_id': vol['VolumeId'], 'resource_type': 'EBS', 'region': region, 'status': vol.get('State'), 'provider': 'AWS', 'metadata': {'size_gb': vol.get('Size'), 'attachments_list': attachments_list, 'attachments': len(vol.get('Attachments', []))}, 'dependencies': [{'type': 'EC2', 'id': attachment['InstanceId']} for attachment in vol.get('Attachments', []) if attachment.get('InstanceId')]})
+            
+            for page in paginator.paginate():
+                for vol in page.get('Volumes', []):
+                    tags = {t['Key']: t['Value'] for t in vol.get('Tags', [])}
+                    name = tags.get('Name', vol['VolumeId'])
+                    
+                    dependencies = []
+                    attachments_list = []
+                    for att in vol.get('Attachments', []):
+                        if 'InstanceId' in att:
+                            attachments_list.append(att['InstanceId'])
+                            dependencies.append(ResourceDependency(type='EC2', id=att['InstanceId']))
+                            
+                    kms_key = vol.get('KmsKeyId')
+                    if kms_key:
+                        dependencies.append(ResourceDependency(type='KMSKey', id=kms_key))
+
+                    res = NormalizedResource(
+                        resource_id=vol['VolumeId'],
+                        resource_type='EBSVolume',
+                        region=region,
+                        name=name,
+                        status=vol.get('State', 'Unknown'),
+                        metadata={
+                            'size_gb': vol.get('Size'),
+                            'volume_type': vol.get('VolumeType'),
+                            'iops': vol.get('Iops'),
+                            'throughput': vol.get('Throughput'),
+                            'snapshot_id': vol.get('SnapshotId'),
+                            'availability_zone': vol.get('AvailabilityZone')
+                        },
+                        security={
+                            'encrypted': vol.get('Encrypted', False),
+                            'kms_key_id': kms_key
+                        },
+                        configuration={
+                            'multi_attach_enabled': vol.get('MultiAttachEnabled', False),
+                            'fast_restored': vol.get('FastRestored', False),
+                            'attachments': attachments_list
+                        },
+                        tags=tags,
+                        dependencies=dependencies
+                    )
+                    volumes.append(res.dict())
+                    
             return volumes
         except Exception:
+            logger.exception('EBS discovery failed for region %s', region)
             return []
--- backend/app/providers/aws/efs.py
+++ backend/app/providers/aws/efs.py
@@ -1,9 +1,10 @@
 import boto3
 import logging
+from app.providers.aws.models import NormalizedResource, ResourceDependency
+
 logger = logging.getLogger(__name__)
 
 class EFSDiscovery:
-
     @staticmethod
     def discover(region: str) -> list[dict]:
         resources = []
@@ -16,13 +17,95 @@ class EFSDiscovery:
                     fs_arn = fs.get('FileSystemArn', '')
                     tags = {t['Key']: t['Value'] for t in fs.get('Tags', [])}
                     name = tags.get('Name', fs_id)
-                    resources.append({'resource_id': fs_arn or fs_id, 'resource_type': 'EFSFileSystem', 'region': region, 'name': name, 'provider': 'AWS', 'metadata': {'arn': fs_arn, 'file_system_id': fs_id, 'lifecycle_state': fs.get('LifeCycleState'), 'performance_mode': fs.get('PerformanceMode', 'generalPurpose'), 'throughput_mode': fs.get('ThroughputMode', 'bursting'), 'provisioned_throughput': fs.get('ProvisionedThroughputInMibps', 0), 'encrypted': fs.get('Encrypted', False), 'kms_key_id': fs.get('KmsKeyId', ''), 'size_bytes': fs.get('SizeInBytes', {}).get('Value', 0), 'tags': tags}, 'dependencies': []})
+                    
+                    dependencies = []
+                    kms_key = fs.get('KmsKeyId')
+                    if kms_key:
+                        dependencies.append(ResourceDependency(type='KMSKey', id=kms_key))
+                        
+                    lifecycle_policies = []
+                    try:
+                        lp_resp = client.describe_lifecycle_configuration(FileSystemId=fs_id)
+                        lifecycle_policies = lp_resp.get('LifecyclePolicies', [])
+                    except Exception: pass
+                    
+                    res = NormalizedResource(
+                        resource_id=fs_arn or fs_id,
+                        resource_type='EFSFileSystem',
+                        region=region,
+                        name=name,
+                        status=fs.get('LifeCycleState', 'Unknown'),
+                        metadata={
+                            'arn': fs_arn,
+                            'file_system_id': fs_id,
+                            'performance_mode': fs.get('PerformanceMode', 'generalPurpose'),
+                            'throughput_mode': fs.get('ThroughputMode', 'bursting'),
+                            'size_bytes': fs.get('SizeInBytes', {}).get('Value', 0)
+                        },
+                        security={
+                            'encrypted': fs.get('Encrypted', False),
+                            'kms_key_id': kms_key
+                        },
+                        configuration={
+                            'provisioned_throughput': fs.get('ProvisionedThroughputInMibps', 0),
+                            'lifecycle_policies': lifecycle_policies
+                        },
+                        tags=tags,
+                        dependencies=dependencies
+                    )
+                    resources.append(res.dict())
+                    
                     try:
                         mt_paginator = client.get_paginator('describe_mount_targets')
                         for mt_page in mt_paginator.paginate(FileSystemId=fs_id):
                             for mt in mt_page.get('MountTargets', []):
                                 mt_id = mt['MountTargetId']
-                                resources.append({'resource_id': mt_id, 'resource_type': 'EFSMountTarget', 'region': region, 'name': f'{name}-mt-{mt_id}', 'provider': 'AWS', 'metadata': {'mount_target_id': mt_id, 'file_system_id': fs_id, 'file_system_arn': fs_arn, 'lifecycle_state': mt.get('LifeCycleState'), 'subnet_id': mt.get('SubnetId', ''), 'vpc_id': mt.get('VpcId', ''), 'ip_address': mt.get('IpAddress', ''), 'network_interface_id': mt.get('NetworkInterfaceId', ''), 'availability_zone': mt.get('AvailabilityZoneName', ''), 'owner_id': mt.get('OwnerId', '')}, 'dependencies': []})
+                                
+                                mt_dependencies = [ResourceDependency(type='EFSFileSystem', id=fs_arn or fs_id)]
+                                subnet_id = mt.get('SubnetId')
+                                if subnet_id:
+                                    mt_dependencies.append(ResourceDependency(type='Subnet', id=subnet_id))
+                                vpc_id = mt.get('VpcId')
+                                if vpc_id:
+                                    mt_dependencies.append(ResourceDependency(type='VPC', id=vpc_id))
+                                eni_id = mt.get('NetworkInterfaceId')
+                                if eni_id:
+                                    mt_dependencies.append(ResourceDependency(type='NetworkInterface', id=eni_id))
+                                    
+                                security_groups = []
+                                try:
+                                    sg_resp = client.describe_mount_target_security_groups(MountTargetId=mt_id)
+                                    security_groups = sg_resp.get('SecurityGroups', [])
+                                    for sg in security_groups:
+                                        mt_dependencies.append(ResourceDependency(type='SecurityGroup', id=sg))
+                                except Exception: pass
+                                
+                                mt_res = NormalizedResource(
+                                    resource_id=mt_id,
+                                    resource_type='EFSMountTarget',
+                                    region=region,
+                                    name=f'{name}-mt-{mt_id}',
+                                    status=mt.get('LifeCycleState', 'Unknown'),
+                                    metadata={
+                                        'mount_target_id': mt_id,
+                                        'file_system_id': fs_id,
+                                        'file_system_arn': fs_arn,
+                                        'ip_address': mt.get('IpAddress', ''),
+                                        'availability_zone': mt.get('AvailabilityZoneName', ''),
+                                        'owner_id': mt.get('OwnerId', '')
+                                    },
+                                    security={
+                                        'security_groups': security_groups
+                                    },
+                                    configuration={
+                                        'subnet_id': subnet_id,
+                                        'vpc_id': vpc_id,
+                                        'network_interface_id': eni_id
+                                    },
+                                    tags={},
+                                    dependencies=mt_dependencies
+                                )
+                                resources.append(mt_res.dict())
                     except Exception:
                         logger.exception('EFS mount targets failed for %s in region %s', fs_id, region)
         except Exception:
